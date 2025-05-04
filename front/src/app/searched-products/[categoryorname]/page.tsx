5805e425c2c9cd76983b73291b29289e2847752c
AnBLarsen <andrealarsen@protonmail.com>
2025-05-04 11:44:52 -0400

    fixing eslint error

--- front/src/app/searched-products/[categoryorname]/page.tsx
+++ front/src/app/searched-products/[categoryorname]/page.tsx
@@ -3,9 +3,13 @@ import Categories from '@/components/categories/Categories';
 import Hero from '@/components/heros/HeroProducts';
 import React from 'react'
 
+type PageProps = {
+  params: {
+    categoryorname: string;
+  };
+};
 
-
-export default async function CategoryOrNamePage({ params }: { params:{ categoryorname: string } }) {
+export default async function CategoryOrNamePage({ params }: PageProps) {
 
   return (
     <>
